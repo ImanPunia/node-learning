35cabcae95102396435e88aa17097cfd2c9a58a7
Imandeep Kaur <imanpunia19@gmail.com>
2021-09-05 15:51:47 +1000

    NL: callbacks and error handling(I)

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var rect =  require('./rectangle');
+const rect =  require('./rectangle');
 
 function solveRectangle(l,b){
     console.log("Solving for rectanfgle for l = " + l + "for b " + b);
-    if(l <= 0 || b <=0){
-        console.log("negative dimensions are not allowed");
-    } else {
-        console.log("area of  rectanglr" + rect.area(l,b));
-        console.log("perimeter ofrectangle  is " + rect.perimeter(l,b))
-    }
+    rect(l,b, (err,rectangle) => {
+        if(err){
+            console.log(err)
+        } else {
+            console.log("area of  rectangle for l " + l + "for b " + b +" = " + rectangle.area());
+            console.log("perimeter ofrectangle  is " + rectangle.perimeter())
+        }
+    });
 }
+
 
 solveRectangle(2,4)
