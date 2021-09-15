f61ebfddb75e5b57265a8a2aa96d19ca8926a93c
Imandeep Kaur <imanpunia19@gmail.com>
2021-09-15 19:40:27 +1000

    NL: basic example of express session I

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -35,58 +35,37 @@ app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-67890=09876-54321'));
 app.use(session({
   name:'session-id',
-  secret: '12345-67890=09876-54321' ,
+  secret: '12345-67890=09876-54321',
   saveUninitialized: false,
   resave:  false,
   store: new FileStore() 
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
-
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-
-    if(!authHeader) {
-      var err= new Error('not authenticated, authorization header not presented');
-      res.setHeader('WWW-authenticate', 'Basic');
-      err.status = 401;
+      var err= new Error('You are not authenticated');
+      err.status = 403;
       return next(err);
-    }
-  
-    var auth =  Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var userName = auth[0];
-    var password  = auth[1];
-  
-    if(userName === 'admin' &&  password === 'password') {
-      req.session.user =  'admin';
-      next();
-    } else {
-      var err= new Error('not authenticated');
-      res.setHeader('WWW-authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   } else {
-    if(req.session.user === 'admin') {
+    if(req.session.user === 'authenticated') {
       next();
     } else {
-      var err= new Error('not authenticated');
-      err.status = 401;
+      var err= new Error('You are not authenticated');
+      err.status = 403;
       return next(err);
     }
-  }
-
-  
+  } 
 }
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 
